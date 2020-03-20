51565a8c27d38c0012e02e30f0feeaffef8098b1
KathrinBeaver <kathrin.beaver@mail.ru>
2020-03-20 10:46:47 +0300

    update

--- seminar19/main.cpp
+++ seminar19/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-#include <memory>
 #include <string>
+#include <exception>
+#include <memory>
 #include <typeinfo>
 #include <vector>
+
+#include "Singleton.cpp"
+#include "SingletonBasic.cpp"
 
 #include "Building_v1.cpp"
 #include "Building_v2.cpp"
@@ -16,66 +20,102 @@
 //using custom deleter for Buildings...
 class Deleter {
 public:
-    void operator() (Building_v1* ptr) {
-        std::string realObject = typeid(*ptr).name();
-        std::cout << "From unique_ptr deleter: deleting ptr of Class: " << realObject << std::endl;
-        if(realObject.find("Circus") != -1) {
-            delete (Circus_v1*)ptr;
-        }
-        else if(realObject.find("University") != -1) {	
-            delete (University*)ptr;
+	void operator() (Building_v1* ptr) {
+		std::string realObject = typeid(*ptr).name();
+		std::cout << "From unique_ptr deleter: deleting ptr of Class: " << realObject << std::endl;
+		if (realObject.find("Circus") != -1) {
+			delete dynamic_cast<Circus_v1*>(ptr);
 		}
-        //if all cases fail - then this is a building
-        else {
-            delete ptr;
+		else if (realObject.find("University") != -1) {
+			delete dynamic_cast<University*>(ptr);
 		}
-    }
+		//if all cases fail - then this is a building
+		else {
+			delete ptr;
+		}
+	}
 };
 
 void example1() {
-    //If destructor is not virtual we still can use smart ptr with Deleter (class or function)
-    Deleter del;    
-    //std::unique_ptr<Building_v1> circus = std::unique_ptr<Building_v1>(new Circus_v1);
-    std::unique_ptr<Building_v1, Deleter> circus = std::unique_ptr<Building_v1, Deleter>(new Circus_v1, del);
+	//If destructor is not virtual we still can use smart ptr with Deleter (class or function)
+	//Deleter del;
+	
+	//std::unique_ptr<Building_v1> circus = std::unique_ptr<Building_v1>(new Circus_v1());
 
-    //Alternative without smart ptr:
-    //Building_v1* circus1 = new Circus_v1();    
-    //delete circus1;
-    //delete (Circus_v1*)circus1;    
+	//std::unique_ptr<Building_v1, Deleter> circus = 
+	//	std::unique_ptr<Building_v1, Deleter>(new Circus_v1, del);
 
-    //If destructor is virtual the right destructor is chosen in runtime
-    std::unique_ptr<Building_v2> circus2 = std::unique_ptr<Building_v2>(new Circus_v11());
+	//Alternative without smart ptr:
+	//Building_v1* circus1 = new Circus_v1();    
+	//delete circus1;
+	//delete (Circus_v1*)circus1;    
+
+	//If destructor is virtual the right destructor is chosen in runtime
+std::unique_ptr<Building_v2> circus2 = std::unique_ptr<Building_v2>(new Circus_v11());
 }
 
 void example2() {
-    std::vector<std::unique_ptr<Building_v2>> town;
-    {
-        Clown cl("Jumbo");
-        town.push_back(std::unique_ptr<Building_v2>(new Circus_v11(&cl)));	
-    }
-    town.push_back(std::unique_ptr<Building_v2>(new University));
+	std::vector<std::unique_ptr<Building_v2>> town;
+	{
+		Clown cl("Jumbo");
+		town.push_back(std::unique_ptr<Building_v2>(new Circus_v11(&cl)));
+	}
+	town.push_back(std::unique_ptr<Building_v2>(new University));
 
-    town[0]->GetInfo();		
-    ((Circus_v11*)town[0].get())->PrintClownStatus();
+	town[0]->GetInfo();
+	static_cast<Circus_v11*>(town[0].get())->PrintClownStatus();
 }
 
 void example2_1() {
-    std::vector<std::unique_ptr<Building_v2>> town;
-    {
-        std::shared_ptr<Clown> cl = std::shared_ptr<Clown>(new Clown("Jumbo"));        
-        town.push_back(std::unique_ptr<Building_v2>(new Circus_v2(cl)));
-    }
-    town.push_back(std::unique_ptr<Building_v2>(new University));
+	std::vector<std::unique_ptr<Building_v2>> town;
+	{
+		std::shared_ptr<Clown> cl = std::shared_ptr<Clown>(new Clown("Jumbo"));
+		town.push_back(std::unique_ptr<Building_v2>(new Circus_v2(cl)));
+	}
+	town.push_back(std::unique_ptr<Building_v2>(new University));
 
-    town[0]->GetInfo();
-    ((Circus_v2*)town[0].get())->PrintClownStatus();
+	town[0]->GetInfo();
+	((Circus_v2*)town[0].get())->PrintClownStatus();
 }
 
+
+class A {
+public:
+	virtual int func() const {
+		return 0;
+	}
+
+	virtual ~A() = 0;
+};
+
+class B : A {
+public:
+//	int func() override {
+	int func() const {
+		return 1;
+	}
+	
+};
+
+class C : B {};
+
+
 int main() {
-    //example1(); //deleters of sub classes
-    example2(); // base class and lifecycle managmnent
-    //example2_1();
-    
+
+//	A a;
+//	A* a = new A();
+//	B b;
+//	C c;
+
+	//example1(); // deleters of sub classes
+	//example2(); // base class and lifecycle managmnent
+	//example2_1();
+
+	//Singleton& s1 = Singleton::Instance();
+	//Singleton& s2 = Singleton::Instance();
+	SingletonBasic* s2 = SingletonBasic::instance();
+	SingletonBasic* s3 = SingletonBasic::instance();
+
 	system("pause");
-    return 0;
+	return 0;
 }
